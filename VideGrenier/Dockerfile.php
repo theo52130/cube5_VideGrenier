a63baa791b8e0366a00238145ce4b2b85c1b682c
theo <37940969+theo52130@users.noreply.github.com>
2025-02-10 18:30:37 +0100

    comments

--- VideGrenier/Dockerfile.php
+++ VideGrenier/Dockerfile.php
@@ -1,4 +1,7 @@
+# Utilise l'image officielle PHP version 8 avec FPM (FastCGI Process Manager)
 FROM php:8-fpm
 
 # Installation des dépendances et de l'extension PDO MySQL
+# RUN exécute des commandes dans le conteneur
 RUN docker-php-ext-install pdo pdo_mysql
+# Installe les extensions PHP PDO et PDO MySQL nécessaires pour interagir avec une base de données MySQL
